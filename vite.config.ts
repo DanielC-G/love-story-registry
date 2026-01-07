1d6c559d2b5f1873c5fbb02a5cde8cb4d0853b92
DanielC-G <36771138+DanielC-G@users.noreply.github.com>
2026-01-07 10:11:33 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/love-story-registry/",
   server: {
     host: "::",
     port: 8080,
